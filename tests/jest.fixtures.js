f6eccacac78955d6da57a49442e1aec7b9262dc2
Come2Daddy <ludovic.landier@gmail.com>
2021-09-06 18:53:54 +0200

    Refactor: removes CLI (tests)

--- tests/jest.fixtures.js
+++ tests/jest.fixtures.js
@@ -4,40 +4,5 @@ module.exports = {
     templateStub: "template.stub",
     pgTable: "jest_pg_upmig",
     migrationFile: "jest-up",
-    jestmigTable: "jest_upmig_test",
-    cli: {
-        _cmds: {
-            "perform": "up",
-            "new": "create",
-            "list": "pending"
-        },
-        globalOptions: [
-            "-V, --version",
-            "-m, --migrations <path>",
-            "-p, --pgtable <table>",
-            "-h, --help"
-        ],
-        commands: [
-            "%__perform__% [options]",
-            "%__new__% [options]",
-            "%__list__% [options]",
-            "help [command]"
-        ],
-        upHelp: [
-            "Perform all pending migrations",
-            "-t, --to <timestamp>",
-            "-s, --steps <number>",
-            "-h, --help"
-        ],
-        newHelp: [
-            "Generate new migration file",
-            "-n, --nosql",
-            "-h, --help"
-        ],
-        listHelp: [
-            "List pending migrations",
-            "-H, --history",
-            "-h, --help"
-        ]
-    }
+    jestmigTable: "jest_upmig_test"
 };
